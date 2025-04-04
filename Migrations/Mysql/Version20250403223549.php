eb4610fda23a5439fd19542e43bf502851162449
robinroloff <116283647+robinroloff@users.noreply.github.com>
2025-04-04 17:20:34 +0200

    Update Migrations/Mysql/Version20250403223549.php
    Co-authored-by: Karsten Dambekalns <karsten@dambekalns.de>

--- Migrations/Mysql/Version20250403223549.php
+++ Migrations/Mysql/Version20250403223549.php
@@ -30,7 +30,6 @@ final class Version20250403223549 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof AbstractMySQLPlatform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\AbstractMySQLPlatform'."
